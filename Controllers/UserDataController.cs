581452137698ff137f078a545835963a9ae41192
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2024-07-15 20:00:44 -0300

    Creating controllers

---
+++ Controllers/UserDataController.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DTO;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Services;
+
+namespace Controllers;
+
+[ApiController]
+[Route("user")]
+public class UserDataController : ControllerBase
+{
+    [HttpPost("register")]
+    [EnableCors("DefaultPolicy")]
+    public async Task<IActionResult> Create(
+        [FromBody]UserDataRegister user,
+        [FromServices]IUserDataService service)
+    {
+        var errors = new List<string>();
+        if(user is null || user.UserName is null)
+            errors.Add("É necessário informar um nome");
+        
+        if(errors.Count > 0)
+            return BadRequest(errors);
+        
+        await service.Create(user);
+        return Ok();
+    }
+
+    [HttpPost("login")]
+    [EnableCors("DefaultPolicy")]
+    public async Task<IActionResult> Login(
+        [FromBody]UserDataLogin user,
+        [FromServices]IUserDataService service,
+        [FromServices]ISecurityService security)
+    {
+        var loggedUser = await service.GetByLogin(user.UserName);
+
+        if(loggedUser is null)
+            return Unauthorized("Usuário não existe");
+
+        var password = await security.HashPassword(loggedUser.UserName, loggedUser.Salt);
+        var realPassword = loggedUser.UserPassword;
+
+        if(password != realPassword)
+            return BadRequest("Senha incorreta");
+        
+        return Ok(user);
+    }
+}
